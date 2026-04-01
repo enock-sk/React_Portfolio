22b2b8ec365b90e196659aff76dc6f83f802bcc1
enock-sk <ekiplangat64@gmail.com>
2026-04-01 13:48:51 +0300

    eslint

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,72 +5,85 @@ import IMG2 from '../../assets/movie.png'
 import IMG3 from '../../assets/portfolio_2.jpg'
 import IMG5 from '../../assets/portfolio_4.jpg'
 import IMG6 from '../../assets/portfolioo.png'
-const data=[
+
+const data = [
   {
-    id:1,
-    image:IMG1,
+    id: 1,
+    image: IMG1,
     title: 'Daily Wallpapers Android App',
     github: 'https://github.com/enock-sk/DailyWallpapers',
     demo: 'https://drive.google.com/file/d/191XUOmwYVVkDzvQa5ucYT0m30sM4CbXv/view?usp=sharing'
   },
   {
-    id:2,
-    image:IMG2,
-    title: 'My Favourite Movie Website in React-Allow Users to Fetch their Favourite Movies',
+    id: 2,
+    image: IMG2,
+    title: 'My Favourite Movie Website in React - Fetch Favourite Movies',
     github: 'https://github.com/enock-sk',
     demo: 'https://favmovie-psi.vercel.app/'
   },
   {
-    id:3,
-    image:IMG3,
-    title: 'JobSpark Django REST API/ Backend',
+    id: 3,
+    image: IMG3,
+    title: 'JobSpark Django REST API / Backend',
     github: 'https://github.com/enock-sk',
     demo: 'https://jobspark-backend.onrender.com/api/jobs/'
   },
   {
-    id:4,
-    image:IMG6,
+    id: 4,
+    image: IMG6,
     title: 'Portfolio Website',
     github: 'https://github.com/enock-sk',
-    demo: 'mrsang.vercel.app'
+    demo: 'https://mrsang.vercel.app'
   },
   {
-    id:5,
-    image:IMG5,
+    id: 5,
+    image: IMG5,
     title: 'Shoe Ecommerce Mobile APP in Flutter',
     github: 'https://github.com/enock-sk',
-    demo: 'http'
+    demo: '#' // temporary placeholder
   },
   {
-    id:6,
-    image:IMG5,
+    id: 6,
+    image: IMG5,
     title: 'Student Springboot REST API',
     github: 'https://github.com',
-    demo: 'http'
+    demo: '#' // temporary placeholder
   }
 ]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
+
       <div className="container portfolio__container">
-        {
-          data.map(({id,image,title,github,demo})=>{
-            return(
-              <article key={id} className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={image} alt={title} />
-          </div>
+        {data.map(({ id, image, title, github, demo }) => (
+          <article key={id} className='portfolio__item'>
+            <div className="portfolio__item-image">
+              <img src={image} alt={title} />
+            </div>
             <h3>{title}</h3>
             <div className='portfolio__item-cta'>
-              <a href={github} className='btn' target='_blank'>Github</a>
-            <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+              <a
+                href={github}
+                className='btn'
+                target='_blank'
+                rel='noreferrer'
+              >
+                Github
+              </a>
+              <a
+                href={demo}
+                className='btn btn-primary'
+                target='_blank'
+                rel='noreferrer'
+              >
+                Live Demo
+              </a>
             </div>
-        </article>
-            )
-          })
-        }
+          </article>
+        ))}
       </div>
     </section>
   )
